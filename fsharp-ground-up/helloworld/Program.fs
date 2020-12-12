be58c689de73c7e323b866658f98199218ed7eaa
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-12-12 13:45:52 +0000

    F# Ground Up - Section 3 completed

---
+++ fsharp-ground-up/helloworld/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let indexedForLoop (argv: string array) = 
+    for i in 0..argv.Length-1 do
+        printfn "Hello %s" argv.[i]
+    ()
+
+let iteratorForLoop(argv: string array) =
+    for person in argv do
+            printfn "Hello %s" person
+    ()
+
+let iterFucnction (argv: string array)=
+    argv
+    |> Array.iter(fun x -> printfn "Hello %s" x)
+
+[<EntryPoint>]
+let main argv =
+    indexedForLoop argv
+    iteratorForLoop argv
+    iterFucnction argv
+    0
